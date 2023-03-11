25404301446481f6423ff1cc55bf79dd3858ae4d
Anoop Reddy <anoop.reddy50@gmail.com>
2023-03-11 16:02:48 -0500

    init commit ✨

--- pacman.js
+++ pacman.js
@@ -280,3 +280,184 @@ Pacman.User = function (game, map) {
     keyMap[KEY.ARROW_UP]    = UP;
     keyMap[KEY.ARROW_RIGHT] = RIGHT;
     keyMap[KEY.ARROW_DOWN]  = DOWN;
+
+    function addScore(nScore) { 
+        score += nScore;
+        if (score >= 10000 && score - nScore < 10000) { 
+            lives += 1;
+        }
+    };
+
+    function theScore() { 
+        return score;
+    };
+
+    function loseLife() { 
+        lives -= 1;
+    };
+
+    function getLives() {
+        return lives;
+    };
+
+    function initUser() {
+        score = 0;
+        lives = 3;
+        newLevel();
+    }
+    
+    function newLevel() {
+        resetPosition();
+        eaten = 0;
+    };
+    
+    function resetPosition() {
+        position = {"x": 90, "y": 120};
+        direction = LEFT;
+        due = LEFT;
+    };
+    
+    function reset() {
+        initUser();
+        resetPosition();
+    };        
+    
+    function keyDown(e) {
+        if (typeof keyMap[e.keyCode] !== "undefined") { 
+            due = keyMap[e.keyCode];
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        }
+        return true;
+	};
+
+    function getNewCoord(dir, current) {   
+        return {
+            "x": current.x + (dir === LEFT && -2 || dir === RIGHT && 2 || 0),
+            "y": current.y + (dir === DOWN && 2 || dir === UP    && -2 || 0)
+        };
+    };
+
+    function onWholeSquare(x) {
+        return x % 10 === 0;
+    };
+
+    function pointToCoord(x) {
+        return Math.round(x/10);
+    };
+    
+    function nextSquare(x, dir) {
+        var rem = x % 10;
+        if (rem === 0) { 
+            return x; 
+        } else if (dir === RIGHT || dir === DOWN) { 
+            return x + (10 - rem);
+        } else {
+            return x - rem;
+        }
+    };
+
+    function next(pos, dir) {
+        return {
+            "y" : pointToCoord(nextSquare(pos.y, dir)),
+            "x" : pointToCoord(nextSquare(pos.x, dir)),
+        };                               
+    };
+
+    function onGridSquare(pos) {
+        return onWholeSquare(pos.y) && onWholeSquare(pos.x);
+    };
+
+    function isOnSamePlane(due, dir) { 
+        return ((due === LEFT || due === RIGHT) && 
+                (dir === LEFT || dir === RIGHT)) || 
+            ((due === UP || due === DOWN) && 
+             (dir === UP || dir === DOWN));
+    };
+
+    function move(ctx) {
+        
+        var npos        = null, 
+            nextWhole   = null, 
+            oldPosition = position,
+            block       = null;
+        
+        if (due !== direction) {
+            npos = getNewCoord(due, position);
+            
+            if (isOnSamePlane(due, direction) || 
+                (onGridSquare(position) && 
+                 map.isFloorSpace(next(npos, due)))) {
+                direction = due;
+            } else {
+                npos = null;
+            }
+        }
+
+        if (npos === null) {
+            npos = getNewCoord(direction, position);
+        }
+        
+        if (onGridSquare(position) && map.isWallSpace(next(npos, direction))) {
+            direction = NONE;
+        }
+
+        if (direction === NONE) {
+            return {"new" : position, "old" : position};
+        }
+        
+        if (npos.y === 100 && npos.x >= 190 && direction === RIGHT) {
+            npos = {"y": 100, "x": -10};
+        }
+        
+        if (npos.y === 100 && npos.x <= -12 && direction === LEFT) {
+            npos = {"y": 100, "x": 190};
+        }
+        
+        position = npos;        
+        nextWhole = next(position, direction);
+        
+        block = map.block(nextWhole);        
+        
+        if ((isMidSquare(position.y) || isMidSquare(position.x)) &&
+            block === Pacman.BISCUIT || block === Pacman.PILL) {
+            
+            map.setBlock(nextWhole, Pacman.EMPTY);           
+            addScore((block === Pacman.BISCUIT) ? 10 : 50);
+            eaten += 1;
+            
+            if (eaten === 182) {
+                game.completedLevel();
+            }
+            
+            if (block === Pacman.PILL) { 
+                game.eatenPill();
+            }
+        }   
+                
+        return {
+            "new" : position,
+            "old" : oldPosition
+        };
+    };
+
+    function isMidSquare(x) { 
+        var rem = x % 10;
+        return rem > 3 || rem < 7;
+    };
+
+    function calcAngle(dir, pos) { 
+        if (dir == RIGHT && (pos.x % 10 < 5)) {
+            return {"start":0.25, "end":1.75, "direction": false};
+        } else if (dir === DOWN && (pos.y % 10 < 5)) { 
+            return {"start":0.75, "end":2.25, "direction": false};
+        } else if (dir === UP && (pos.y % 10 < 5)) { 
+            return {"start":1.25, "end":1.75, "direction": true};
+        } else if (dir === LEFT && (pos.x % 10 < 5)) {             
+            return {"start":0.75, "end":1.25, "direction": true};
+        }
+        return {"start":0, "end":2, "direction": false};
+    };
+
+   
